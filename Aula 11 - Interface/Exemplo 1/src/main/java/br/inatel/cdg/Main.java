faf2a1c1ec4c2e59f79d8e4cd745c11a127065ee
lucasdemartha <lucasribm@gmail.com>
2022-04-29 13:58:52 -0300

    att

---
+++ Aula 11 - Interface/Exemplo 1/src/main/java/br/inatel/cdg/Main.java
@@ -0,0 +1,11 @@
+package br.inatel.cdg;
+
+import br.inatel.cdg.animais.Humano;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Humano h1 = new Humano();
+        h1.comer();
+    }
+}
