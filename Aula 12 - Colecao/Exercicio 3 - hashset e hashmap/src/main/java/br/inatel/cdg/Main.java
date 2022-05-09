85e13bea3cb144689d37d71de47ab9c9a969136d
lucasdemartha <lucasribm@gmail.com>
2022-05-09 18:22:01 -0300

    tempo

---
+++ Aula 12 - Colecao/Exercicio 3 - hashset e hashmap/src/main/java/br/inatel/cdg/Main.java
@@ -0,0 +1,75 @@
+package br.inatel.cdg;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numValores = 100000;
+        long tempoInicial, tempoFinal;
+
+        List<Integer> listaInteiro = new ArrayList<Integer>();
+        Set<Integer> conjuntoInteiro = new HashSet<Integer>();
+        Map<Integer, Integer> mapaInteiro = new HashMap<Integer, Integer>();
+
+        //Inserindo na List (ArrayList)
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            listaInteiro.add(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para inserir na List: "
+                + (tempoFinal-tempoInicial)+"ms");
+
+        //Inserindo no Set (HashSet)
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            conjuntoInteiro.add(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para inserir no Set: "
+                + (tempoFinal-tempoInicial)+"ms");
+
+        //Inserindo no Set (HashMap)
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            mapaInteiro.put(i, i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para inserir no Map: "
+                + (tempoFinal-tempoInicial)+"ms");
+
+        //Fazendo a busca
+        //Buscando na List (ArrayList)
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            listaInteiro.contains(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para buscar na List: "
+                + (tempoFinal-tempoInicial)+"ms");
+
+        //Buscando no HashSet
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            conjuntoInteiro.contains(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para buscar no Set: "
+                + (tempoFinal-tempoInicial)+"ms");
+
+        //Buscando no HashMap
+        tempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < numValores; i++) {
+            mapaInteiro.containsKey(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo para buscar no Map: "
+                + (tempoFinal-tempoInicial)+"ms");
+    }
+}
